e793b9223324df6b5eccc5c521db627b4573aa3a
valeria <valeria.orehovaaa@mail.ru>
2026-01-21 00:28:00 +0300

    add main page and links

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,18 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import MainPage from './components/MainPage'
 import Navbar from "./components/Navbar"
 
 function App() {
 
   return (
-    <Navbar/>
+    <><Navbar/>
+    <Routes>
+      <Route path='/' element={<MainPage/>}></Route>
+    
+    
+    </Routes>
+    </>
   )
 }
 
--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,11 +1,13 @@
+import { Link } from "react-router-dom";
+
 export default function Navbar(){
     return(
-        <div id="conteinerNav" className="sticky inset-0">
-            <div id="navbar" className="flex justify-between items-center p-8 bg-pink-200">
-                <div id="logoNav" className="text-3xl font-bold cursor-pointer">To Do</div>
-                <div id="linksNav" className="w-2/5 flex justify-between text-2xl">
-                    <div className="linkNav cursor-pointer">Блокноты</div>
-                    <div className="linkNav cursor-pointer">Профиль</div>
+        <div className="sticky inset-0">
+            <div className="flex justify-between items-center p-8 bg-pink-200">
+                <div className="text-3xl font-bold"><Link to="/">To Do</Link></div>
+                <div className="w-2/5 flex justify-between text-2xl">
+                    <div className="linkNav"><Link to="/notepads">Блокноты</Link></div>
+                    <div className="linkNav"><Link to="/lk">Профиль</Link></div>
                 </div>
             </div>
         </div>
